d2c4d3eee865de875f00f8a8177f45391a4dca0e
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-10-08 18:15:25 -0300

    Update HomePaciente

--- src/pages/Paciente/HomePaciente/index.jsx
+++ src/pages/Paciente/HomePaciente/index.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react"
-import { FaStar, FaUserCircle } from "react-icons/fa"
-import { HiDocumentReport } from "react-icons/hi"
-import { IoBarChart, IoChatbox, IoFastFood } from "react-icons/io5"
-import { MdFlatware } from "react-icons/md"
-import { Link } from "react-router-dom"
-import BarsChart from "../../../components/Grafico"
-import styles from "./HomePaciente.module.css"
+import { useState } from "react";
+import { FaStar, FaUserCircle } from "react-icons/fa";
+import { HiDocumentReport } from "react-icons/hi";
+import { IoBarChart, IoChatbox, IoFastFood } from "react-icons/io5";
+import { MdFlatware } from "react-icons/md";
+import { Link } from "react-router-dom";
+import BarsChart from "../../../components/Grafico";
+import styles from "./HomePaciente.module.css";
+import { IoChevronForward } from 'react-icons/io5';
+import { SiGoogleforms } from "react-icons/si";
+
 
 function HomePaciente() {
   const [userMenuOpen, setUserMenuOpen] = useState(false)
@@ -82,6 +85,14 @@ function HomePaciente() {
             </Link>
           </li>
           <li>
+            <Link to="/questionarioPaciente">
+              <div className={styles.linkSlider}>
+                <SiGoogleforms className={styles.icon} />
+                <p>Questionários</p>
+              </div>
+            </Link>
+          </li>
+          <li>
             <Link to="/receitasFavoritasPaciente">
               <div className={styles.linkSlider}>
                 <FaStar className={styles.icon} />
@@ -101,26 +112,57 @@ function HomePaciente() {
       </nav>
 
       <main>
-        <div className={styles.testeHomeP}>
-          <div>
-            <img src="\Img_Home.png" alt="Teste" />
+        <div className={styles.planoAlimentarCard}>
+          <img src="/Img_Home.png" alt="Plano Alimentar" className={styles.cardImage} />
+          <div className={styles.cardContent}>
+            <h3>Plano alimentar</h3>
+            <p>Acompanhe todo o planejamento feito pelo seu Nutricionista.</p>
+            <p className={styles.nutriName}>Prescrição feita por: Victor Santos</p>
+          </div>
+          <div className={styles.arrowIcon}>
+            <IoChevronForward size={40} />
           </div>
         </div>
+
         <div className={styles.mainContent}>
-          <section className={styles.section}>
+        {/* 1 */}
+          <section className={styles.section01}>
             <h2>Próximas refeições</h2>
             <div className={styles.cards}>
               <div className={styles.card}>
-                <p>Lanche da manhã</p>
+                <p>Café da Manhã</p>
+                <p>7h</p>
+              </div>
+
+              <div className={styles.card}>
+                <p>Lanche da Manhã</p>
                 <p>10h</p>
               </div>
+
               <div className={styles.card}>
                 <p>Almoço</p>
                 <p>13h</p>
               </div>
+
+              <div className={styles.card}>
+                <p>Lanche da Tarde</p>
+                <p>15h30</p>
+              </div>
+
+              <div className={styles.card}>
+                <p>Jantar</p>
+                <p>19h</p>
+              </div>
+
+              <div className={styles.card}>
+                <p>Ceia</p>
+                <p>21h</p>
+              </div>
             </div>
           </section>
-          <section className={styles.section}>
+
+          {/* 2 */}
+          <section className={styles.section02}>
             <h2>Meu progresso</h2>
             <div className={styles.progressCard}>
               <p>Peso Corporal</p>
@@ -137,7 +179,9 @@ function HomePaciente() {
               </div>
             </div>
           </section>
-          <section className={styles.section}>
+
+          {/* 3 */}
+          <section className={styles.section03}>
             <h2>Próxima consulta</h2>
             <div className={styles.consultationCard}>
               <p>Domingo | 18/08/2024</p>
