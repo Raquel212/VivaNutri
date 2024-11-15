f9b0fe7f19167539db67a1f6dad548e02b7a9b86
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-11-15 14:25:12 -0300

    Update notificacao

--- src/pages/GerenciarClientesPsico/index.jsx
+++ src/pages/GerenciarClientesPsico/index.jsx
@@ -44,7 +44,7 @@ function GerenciarClientesPsico() {
           <FaUserCircle onClick={toggleUserMenu} className={styles.userIcon} />
           {userMenuOpen && (
             <div className={styles.dropdownMenu}>
-              <Link to="/notifications" className={styles.linkHome}>
+              <Link to="/notificationsPsico" className={styles.linkHome}>
                 Notificações
               </Link>
               <Link to="/EditarPsico" className={styles.linkHome}>
