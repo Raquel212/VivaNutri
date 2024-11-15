98168d272c7f76ea5eabf3820f63567fc8d3d32a
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-11-15 14:30:05 -0300

    Update notificacao

--- src/pages/Paciente/ChatPaciente/ChatPaciente.jsx
+++ src/pages/Paciente/ChatPaciente/ChatPaciente.jsx
@@ -157,7 +157,7 @@ function ChatPaciente() {
           <FaUserCircle onClick={toggleUserMenu} className={styles.userIcon} />
           {userMenuOpen && (
             <div className={styles.dropdownMenu}>
-              <Link to="/notifications" className={styles.linkHome}>
+              <Link to="/notificationsPaci" className={styles.linkHome}>
                 Notificações
               </Link>
               <Link to="/receitasFavoritasPaciente" className={styles.linkHome}>
