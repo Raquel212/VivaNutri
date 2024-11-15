823a8819af821af2e9313b1c512017a0dcf10ef1
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-11-15 14:31:48 -0300

    Update notificacao

--- src/pages/Paciente/RefeicoesPaciente/RefeicaoPaciente.jsx
+++ src/pages/Paciente/RefeicoesPaciente/RefeicaoPaciente.jsx
@@ -133,7 +133,7 @@ function RefeicaoPaciente() {
           <FaUserCircle onClick={toggleUserMenu} className={styles.userIcon} />
           {userMenuOpen && (
             <div className={styles.dropdownMenu}>
-              <Link to="/notifications" className={styles.linkHome}>
+              <Link to="/notificationsPaci" className={styles.linkHome}>
                 Notificações
               </Link>
               <Link to="/receitasFavoritasPaciente" className={styles.linkHome}>
