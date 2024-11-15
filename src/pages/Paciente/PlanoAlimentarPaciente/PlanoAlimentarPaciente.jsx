38e2a22f30b5b3130bbaaf487bf7398c6b54b46e
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-11-15 14:30:59 -0300

    Update notificacao

--- src/pages/Paciente/PlanoAlimentarPaciente/PlanoAlimentarPaciente.jsx
+++ src/pages/Paciente/PlanoAlimentarPaciente/PlanoAlimentarPaciente.jsx
@@ -125,7 +125,7 @@ function PlanoAlimentarPaciente() {
           <FaUserCircle onClick={toggleUserMenu} className={styles.userIcon} />
           {userMenuOpen && (
             <div className={styles.dropdownMenu}>
-              <Link to="/notifications" className={styles.linkHome}>
+              <Link to="/notificationsPaci" className={styles.linkHome}>
                 Notificações
               </Link>
               <Link to="/receitasFavoritasPaciente" className={styles.linkHome}>
