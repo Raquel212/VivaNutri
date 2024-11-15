1b817187477df23286c93de4a0fde6abbe4cb77c
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2024-11-15 14:26:57 -0300

    Update notificacao

--- src/pages/HomePsico/index.jsx
+++ src/pages/HomePsico/index.jsx
@@ -28,7 +28,7 @@ function HomePsico() {
           <FaUserCircle onClick={toggleUserMenu} className={styles.userIcon} />
           {userMenuOpen && (
             <div className={styles.dropdownMenu}>
-              <Link to="/notifications" className={styles.linkHome}>
+              <Link to="/notificationsPsico" className={styles.linkHome}>
                 Notificações
               </Link>
               <Link to="/EditarPsico" className={styles.linkHome}>
